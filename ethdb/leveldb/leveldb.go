42b952c202e3cb50939ac00bdf96069038bc8563
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-08-20 16:10:03 -0400

    Fixed small stuff around database disable compaction

--- ethdb/leveldb/leveldb.go
+++ ethdb/leveldb/leveldb.go
@@ -104,11 +104,6 @@ func New(file string, cache int, handles int, namespace string) (*Database, erro
 		DisableSeeksCompaction: true,
 	}
 
-	// DM: Having it currently causes an import cycle in tests because trie/trie_test.go depends on ethdb/leveldb/leveldb.go
-	//     which depends on deepmind/context.go which depends on core/types in which core/types/derive_sha.go depends on trie
-	//     which creates the cycle. The problem is fixed in later version of Geth, since compaction is not that a problem (and
-	//     because some better extensive data point are required), we leave like that until we are abe to re-activate in future
-	//     version of Geth.
 	if deepmind.CompactionDisabled {
 		// By setting those values really high, we disable compaction of the database completely
 		maxInt := int(^uint(0) >> 1)
@@ -232,14 +227,9 @@ func (db *Database) Stat(property string) (string, error) {
 // is treated as a key after all keys in the data store. If both is nil then it
 // will compact entire data store.
 func (db *Database) Compact(start []byte, limit []byte) error {
-	// DM: Having it currently causes an import cycle in tests because trie/trie_test.go depends on ethdb/leveldb/leveldb.go
-	//     which depends on deepmind/context.go which depends on core/types in which core/types/derive_sha.go depends on trie
-	//     which creates the cycle. The problem is fixed in later version of Geth, since compaction is not that a problem (and
-	//     because some better extensive data point are required), we leave like that until we are abe to re-activate in future
-	//     version of Geth.
-	// if deepmind.CompactionDisabled {
-	// 	return nil
-	// }
+	if deepmind.CompactionDisabled {
+		return nil
+	}
 
 	return db.db.CompactRange(util.Range{Start: start, Limit: limit})
 }
