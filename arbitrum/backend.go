516f0ce1dbfd030289ae76d55dee57021d12d320
Tsahi Zidenberg <tsahi@offchainlabs.com>
2021-11-14 15:10:20 +0200

    arbitrum: cleanup backend
    use our own config, remove unnecessary fields

--- arbitrum/backend.go
+++ arbitrum/backend.go
@@ -1,24 +1,19 @@
 package arbitrum
 
 import (
-	"math/big"
-
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/eth/ethconfig"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/ethereum/go-ethereum/node"
 )
 
 type Backend struct {
-	publisher   TransactionPublisher
-	stack       *node.Node
-	chainId     *big.Int
-	apiBackend  *APIBackend
-	ethConfig   *ethconfig.Config
-	ethDatabase ethdb.Database
-	inboxDb     ethdb.Database
+	publisher  TransactionPublisher
+	stack      *node.Node
+	apiBackend *APIBackend
+	config     *Config
+	chainDb    ethdb.Database
 
 	txFeed event.Feed
 	scope  event.SubscriptionScope
@@ -28,14 +23,26 @@ type Backend struct {
 	chanNewBlock chan struct{} //create new L2 block unless empty
 }
 
-func NewBackend(stack *node.Node, config *ethconfig.Config, ethDatabase ethdb.Database, inboxDb ethdb.Database, blockChain *core.BlockChain, chainId *big.Int, publisher TransactionPublisher) (*Backend, error) {
+type Config struct {
+	// RPCGasCap is the global gas cap for eth-call variants.
+	RPCGasCap uint64
+
+	// RPCTxFeeCap is the global transaction fee(price * gaslimit) cap for
+	// send-transction variants. The unit is ether.
+	RPCTxFeeCap float64
+}
+
+var DefaultConfig = Config{
+	RPCGasCap:   50000000,
+	RPCTxFeeCap: 1, // 1 ether
+}
+
+func NewBackend(stack *node.Node, config *Config, chainDb ethdb.Database, blockChain *core.BlockChain, publisher TransactionPublisher) (*Backend, error) {
 	backend := &Backend{
 		publisher:    publisher,
 		stack:        stack,
-		chainId:      chainId,
-		ethConfig:    config,
-		ethDatabase:  ethDatabase,
-		inboxDb:      inboxDb,
+		config:       config,
+		chainDb:      chainDb,
 		chanTxs:      make(chan *types.Transaction, 100),
 		chanClose:    make(chan struct{}, 1),
 		chanNewBlock: make(chan struct{}, 1),
@@ -50,8 +57,8 @@ func (b *Backend) APIBackend() *APIBackend {
 	return b.apiBackend
 }
 
-func (b *Backend) InboxDb() ethdb.Database {
-	return b.inboxDb
+func (b *Backend) ChainDb() ethdb.Database {
+	return b.chainDb
 }
 
 func (b *Backend) EnqueueL2Message(tx *types.Transaction) error {
