a1cc4c1f1a9d2bbe723fe4bf32eef0d839ddb4eb
Harry Kalodner <harry@offchainlabs.com>
2021-12-08 23:32:06 -0500

    Extract modification into separate file

--- accounts/abi/bind/backends/simulated.go
+++ accounts/abi/bind/backends/simulated.go
@@ -793,7 +793,6 @@ type callMsg struct {
 	ethereum.CallMsg
 }
 
-func (m callMsg) UnderlyingTransaction() *types.Transaction { return nil }
 func (m callMsg) From() common.Address         { return m.CallMsg.From }
 func (m callMsg) Nonce() uint64                { return 0 }
 func (m callMsg) IsFake() bool                 { return true }
