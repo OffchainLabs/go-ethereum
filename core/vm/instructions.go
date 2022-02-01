6cdef3a4b6dc4282b7b304a5155a45ab2d80e1b6
Lee Bousfield <ljbousfield@gmail.com>
2022-02-01 10:40:05 -0600

    Always call AddBalance

--- core/vm/instructions.go
+++ core/vm/instructions.go
@@ -799,13 +799,12 @@ func opStop(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) ([]byt
 func opSuicide(pc *uint64, interpreter *EVMInterpreter, scope *ScopeContext) ([]byte, error) {
 	beneficiary := scope.Stack.pop()
 	balance := interpreter.evm.StateDB.GetBalance(scope.Contract.Address())
+	interpreter.evm.StateDB.AddBalance(beneficiary.Bytes20(), balance)
+	interpreter.evm.StateDB.Suicide(scope.Contract.Address())
 	if beneficiary.Bytes20() == scope.Contract.Address() {
 		// Arbitrum: calling selfdestruct(this) burns the balance
 		interpreter.evm.StateDB.ExpectBalanceBurn(balance)
-	} else {
-		interpreter.evm.StateDB.AddBalance(beneficiary.Bytes20(), balance)
 	}
-	interpreter.evm.StateDB.Suicide(scope.Contract.Address())
 	if interpreter.cfg.Debug {
 		interpreter.cfg.Tracer.CaptureEnter(SELFDESTRUCT, scope.Contract.Address(), beneficiary.Bytes20(), []byte{}, 0, balance)
 		interpreter.cfg.Tracer.CaptureExit([]byte{}, 0, nil)
