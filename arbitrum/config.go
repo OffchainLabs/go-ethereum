d43a811ea32453b8d8406f6dbe878ff01082aabb
Joshua Colvin <jcolvin@offchainlabs.com>
2022-03-10 17:47:37 -0700

    Store RPCGasCap as uint64 but update as signed
    RPCGasCap is stored as uint64, but default value and value read from configuration should be treated as signed so that it can be recast as int when needed

--- arbitrum/config.go
+++ arbitrum/config.go
@@ -9,6 +9,7 @@ import "time"
 
 type Config struct {
 	// RPCGasCap is the global gas cap for eth-call variants.
+	// Store as uint64, but default and value read from config as signed so that it can be cast to int.
 	RPCGasCap uint64
 
 	// RPCTxFeeCap is the global transaction fee(price * gaslimit) cap for
@@ -20,13 +21,13 @@ type Config struct {
 }
 
 func ConfigAddOptions(prefix string, f *flag.FlagSet) {
-	f.Uint64(prefix+".rpc-gas-cap", DefaultConfig.RPCGasCap, "gas cap for eth-call variants")
+	f.Int64(prefix+".rpc-gas-cap", int64(DefaultConfig.RPCGasCap), "gas cap for eth-call variants")
 	f.Float64(prefix+".rpc-tx-fee-cap", DefaultConfig.RPCTxFeeCap, "transaction fee cap for send-transaction variants in ETH")
 	f.Duration(prefix+".rpc-evm-timeout", DefaultConfig.RPCEVMTimeout, "timeout for eth-call")
 }
 
 var DefaultConfig = Config{
-	RPCGasCap:     math.MaxUint64, // no real limit, can cast to int
-	RPCTxFeeCap:   1,              // 1 ether
-	RPCEVMTimeout: 0,              // disable timeout
+	RPCGasCap:     math.MaxInt64, // no real limit, can cast to int
+	RPCTxFeeCap:   1,             // 1 ether
+	RPCEVMTimeout: 0,             // disable timeout
 }
