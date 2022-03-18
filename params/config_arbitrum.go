43f5de2610f60e8656e61486eb76250f2783c4cb
Lee Bousfield <ljbousfield@gmail.com>
2022-03-18 14:02:19 -0500

    Add new ArbitrumDevTestDAS chain config

--- params/config_arbitrum.go
+++ params/config_arbitrum.go
@@ -65,6 +65,15 @@ func ArbitrumDevTestParams() ArbitrumChainParams {
 	}
 }
 
+func ArbitrumDevTestDASParams() ArbitrumChainParams {
+	return ArbitrumChainParams{
+		EnableArbOS:               true,
+		AllowDebugPrecompiles:     true,
+		DataAvailabilityCommittee: true,
+		InitialChainOwner:         common.Address{},
+	}
+}
+
 func DisableArbitrumParams() ArbitrumChainParams {
 	return ArbitrumChainParams{
 		EnableArbOS:               false,
@@ -126,7 +135,7 @@ func ArbitrumTestnetChainConfig() *ChainConfig {
 
 func ArbitrumDevTestChainConfig() *ChainConfig {
 	return &ChainConfig{
-		ChainID:             big.NewInt(412345),
+		ChainID:             big.NewInt(412346),
 		HomesteadBlock:      big.NewInt(0),
 		DAOForkBlock:        nil,
 		DAOForkSupport:      true,
@@ -149,8 +158,34 @@ func ArbitrumDevTestChainConfig() *ChainConfig {
 	}
 }
 
+func ArbitrumDevTestDASChainConfig() *ChainConfig {
+	return &ChainConfig{
+		ChainID:             big.NewInt(412347),
+		HomesteadBlock:      big.NewInt(0),
+		DAOForkBlock:        nil,
+		DAOForkSupport:      true,
+		EIP150Block:         big.NewInt(0),
+		EIP150Hash:          common.Hash{},
+		EIP155Block:         big.NewInt(0),
+		EIP158Block:         big.NewInt(0),
+		ByzantiumBlock:      big.NewInt(0),
+		ConstantinopleBlock: big.NewInt(0),
+		PetersburgBlock:     big.NewInt(0),
+		IstanbulBlock:       big.NewInt(0),
+		MuirGlacierBlock:    big.NewInt(0),
+		BerlinBlock:         big.NewInt(0),
+		LondonBlock:         big.NewInt(0),
+		ArbitrumChainParams: ArbitrumDevTestDASParams(),
+		Clique: &CliqueConfig{
+			Period: 0,
+			Epoch:  0,
+		},
+	}
+}
+
 var ArbitrumSupportedChainConfigs = []*ChainConfig{
 	ArbitrumOneChainConfig(),
 	ArbitrumTestnetChainConfig(),
 	ArbitrumDevTestChainConfig(),
+	ArbitrumDevTestDASChainConfig(),
 }
