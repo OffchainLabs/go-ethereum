808a287970958d94513b3521c91962b59b21bac6
Matthieu Vachon <matt@streamingfast.io>
2022-04-27 13:20:44 -0400

    Fixed new required params

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -867,6 +867,8 @@ func DoCall(ctx context.Context, b Backend, args CallArgs, blockNrOrHash rpc.Blo
 			msg.GasPrice(),
 			msg.Nonce(),
 			msg.Data(),
+			nil,
+			0,
 		)
 		dmContext.RecordTrxFrom(msg.From())
 	}
