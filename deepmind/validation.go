c73d825c656a2a72fc80b297a95fd6d373b49bc3
Matthieu Vachon <matt@streamingfast.io>
2022-05-03 13:14:11 -0400

    Added transaction type runtime initialization check

---
+++ deepmind/validation.go
@@ -0,0 +1,89 @@
+package deepmind
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/rlp"
+)
+
+var errFirehoseUnknownType = errors.New("firehose unknown tx type")
+var sanitizeRegexp = regexp.MustCompile(`[\t\n(\s){2,}]+`)
+
+func init() {
+	firehoseKnownTxTypes := map[byte]bool{types.LegacyTxType: true, types.AccessListTxType: true}
+
+	for txType := byte(0); txType < 255; txType++ {
+		err := validateFirehoseKnownTransactionType(txType, firehoseKnownTxTypes[txType])
+		if err != nil {
+			panic(fmt.Errorf(sanitizeRegexp.ReplaceAllString(`
+				If you see this panic message, it comes from a sanity check of Firehose instrumentation
+				around Ethereum transaction types.
+
+				Over time, Ethereum added new transaction types but there is no easy way for Firehose to
+				report a compile time check that a new transaction's type must be handled. As such, we
+				have a runtime check at initialization of the process that encode/decode each possible
+				transaction's receipt and check proper handling.
+
+				This panic means that a transaction that Firehose don't know about has most probably
+				be added and you must take great care to instrument it. One of the most important place
+				to look is in 'deepmind.StartTransaction' where it should be properly handled. Think
+				carefully, read the EIP and ensure that any new "semantic" the transactions type's is
+				bringing is handled and instrumented.
+
+				For example, when London fork appeared, semantic of 'GasPrice' change and it required
+				a different computation for 'GasPrice' and 'DynamicFeeTx' transaction were added.
+
+				It's also possible the test itself here is now flaky, we do 'types.Receipt{Type: <type>}'
+				then encode it through RLP and then decode it. This should catch potential new transaction
+				types but could be now generate false positive.
+			`, " ")))
+		}
+	}
+}
+
+func validateFirehoseKnownTransactionType(txType byte, isKnownFirehoseTxType bool) error {
+	writerBuffer := bytes.NewBuffer(nil)
+
+	receipt := types.Receipt{Type: txType}
+	err := receipt.EncodeRLP(writerBuffer)
+	if err != nil {
+		if err == types.ErrTxTypeNotSupported {
+			if isKnownFirehoseTxType {
+				return fmt.Errorf("firehose known type but encoding RLP of receipt led to 'types.ErrTxTypeNotSupported'")
+			}
+
+			// It's not a known type and encoding reported the same, so validation is OK
+			return nil
+		}
+
+		// All other cases results in an error as we should have been able to encode it to RLP
+		return fmt.Errorf("encoding RLP: %w", err)
+	}
+
+	readerBuffer := bytes.NewBuffer(writerBuffer.Bytes())
+	err = receipt.DecodeRLP(rlp.NewStream(readerBuffer, 0))
+	if err != nil {
+		if err == types.ErrTxTypeNotSupported {
+			if isKnownFirehoseTxType {
+				return fmt.Errorf("firehose known type but decoding of RLP of receipt led to 'types.ErrTxTypeNotSupported'")
+			}
+
+			// It's not a known type and decoding reported the same, so validation is OK
+			return nil
+		}
+
+		// All other cases results in an error as we should have been able to decode it from RLP
+		return fmt.Errorf("decoding RLP: %w", err)
+	}
+
+	// If we reach here, encoding/decoding accepted the transaction's type, so let's ensure we expected the same
+	if !isKnownFirehoseTxType {
+		return errFirehoseUnknownType
+	}
+
+	return nil
+}
