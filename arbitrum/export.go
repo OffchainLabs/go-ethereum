4d987bccf94a53cc3533162bdff3bd96850a52c9
Rachel Franks <nfranks@protonmail.com>
2022-05-31 13:22:49 -0500

    export DoEstimateGas

---
+++ arbitrum/export.go
@@ -0,0 +1,15 @@
+package arbitrum
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/internal/ethapi"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+type TransactionArgs = ethapi.TransactionArgs
+
+func EstimateGas(ctx context.Context, b ethapi.Backend, args TransactionArgs, blockNrOrHash rpc.BlockNumberOrHash, gasCap uint64) (hexutil.Uint64, error) {
+	return ethapi.DoEstimateGas(ctx, b, args, blockNrOrHash, gasCap)
+}
