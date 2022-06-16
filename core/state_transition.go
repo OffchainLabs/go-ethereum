3c799d820f4e9c3642947dcc75d579ca1f8cd9ad
Lee Bousfield <ljbousfield@gmail.com>
2022-06-15 21:29:29 -0500

    Fix tips on non-Arbitrum chains

--- core/state_transition.go
+++ core/state_transition.go
@@ -302,7 +302,7 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	// 6. caller has enough balance to cover asset transfer for **topmost** call
 
 	// Arbitrum: drop support for tips from upgrade 2 onward
-	if st.gasPrice.Cmp(st.evm.Context.BaseFee) > 0 {
+	if st.evm.ChainConfig().IsArbitrum() && st.gasPrice.Cmp(st.evm.Context.BaseFee) > 0 {
 		st.gasPrice = st.evm.Context.BaseFee
 		st.gasTipCap = common.Big0
 	}
