2ffdc0614abe7feedfd87a8d272f3b5b49caac49
Matthieu Vachon <matt@streamingfast.io>
2022-08-16 17:51:23 -0400

    Fixed end block data marshalling for `finalizedBlockNum`

--- deepmind/context_test.go
+++ deepmind/context_test.go
@@ -2,10 +2,12 @@ package deepmind
 
 import (
 	"encoding/hex"
+	"math/big"
 	"regexp"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -109,4 +111,22 @@ func hash(t *testing.T, in string) common.Hash {
 	t.Helper()
 
 	return common.HexToHash(in)
+}
+
+func TestEndBlock_DataMarshaling(t *testing.T) {
+	header := &types.Header{}
+	uncles := []*types.Block{}
+	finalizedBlock := types.NewBlockWithHeader(&types.Header{
+		Number: big.NewInt(2048),
+	})
+
+	endData := map[string]interface{}{
+		"header":             header,
+		"uncles":             uncles,
+		"totalDifficulty":    (*hexutil.Big)(big.NewInt(1024)),
+		"finalizedBlockNum":  (*hexutil.Big)(finalizedBlock.Header().Number),
+		"finalizedBlockHash": finalizedBlock.Header().Hash(),
+	}
+
+	require.Equal(t, `{"finalizedBlockHash":"0x38b1c79e3acb45df1ac1fbd4d70e08c655874fc592c5c86342a29baf4f001769","finalizedBlockNum":"0x800","header":{"parentHash":"0x0000000000000000000000000000000000000000000000000000000000000000","sha3Uncles":"0x0000000000000000000000000000000000000000000000000000000000000000","miner":"0x0000000000000000000000000000000000000000","stateRoot":"0x0000000000000000000000000000000000000000000000000000000000000000","transactionsRoot":"0x0000000000000000000000000000000000000000000000000000000000000000","receiptsRoot":"0x0000000000000000000000000000000000000000000000000000000000000000","logsBloom":"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000","difficulty":null,"number":null,"gasLimit":"0x0","gasUsed":"0x0","timestamp":"0x0","extraData":"0x","mixHash":"0x0000000000000000000000000000000000000000000000000000000000000000","nonce":"0x0000000000000000","baseFeePerGas":null,"hash":"0xc3bd2d00745c03048a5616146a96f5ff78e54efb9e5b04af208cdaff6f3830ee"},"totalDifficulty":"0x400","uncles":[]}`, JSON(endData))
 }
