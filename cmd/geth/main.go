da7658cb2f1e3aeebf7ce8cf7118c52b2aa55791
Tristan Wilson <twilson@offchainlabs.com>
2022-09-22 18:13:14 -0700

    Add CLI opt to disable RPC metric histogram reset
    All RPC methods produce a metric called rpc/duration/methodname/success
    and /failure. This metric is implemented as an exponential forward
    decaying sampler, and prior to this change the sampler was always
    cleared when the metrics endpoint was scraped, using
    metrics.ResettingSampler. There is a note in resetting_sample.go that
    this is done to avoid having metrics for low frequence events be skewed
    by old samples, and that this may "break for multi-monitor systems".
    
    This change adds a command line option to turn off resetting the sampler
    for these metrics. For installations with a high scrape rate of the
    metrics endpoint, if the sampler is reset every scrape then this skews
    results by finding the quantiles over too small a set of samples, so a
    simple way of turning off the reset would be an improvement. The
    old behavior is preserved by default so as not to break any
    installations that have come to depend on this behavior.
    
    Ultimately to get good running approximations of the quantiles the
    reservoir size of the sampler would be tunable for each event, based on
    the expected frequency of that event, and the time period the quantile
    was desired to be calculated over (some RPC methods get more traffic
    than others). This is, however, too much confiuration to expose at the
    command line, so this change just exposes on/off for resetting the
    samplers.

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -189,6 +189,7 @@ var (
 	metricsFlags = []cli.Flag{
 		utils.MetricsEnabledFlag,
 		utils.MetricsEnabledExpensiveFlag,
+		utils.MetricsDisableRPCHistogramResetFlag,
 		utils.MetricsHTTPFlag,
 		utils.MetricsPortFlag,
 		utils.MetricsEnableInfluxDBFlag,
