31e9c0c7ff57f8a93e85aa415fd411835adedd0a
Tristan Wilson <twilson@offchainlabs.com>
2023-06-05 17:37:36 -0700

    Fix Receipt ContractAddress DeriveFields merge err

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -420,10 +420,12 @@ func (rs Receipts) DeriveFields(config *params.ChainConfig, hash common.Hash, nu
 
 		if rs[i].Type != ArbitrumLegacyTxType {
 			// The contract address can be derived from the transaction itself
-			if txs[i].To() == nil && rs[i].ContractAddress == (common.Address{}) {
-				// Deriving the signer is expensive, only do if it's actually needed
-				from, _ := Sender(signer, txs[i])
-				rs[i].ContractAddress = crypto.CreateAddress(from, txs[i].Nonce())
+			if txs[i].To() == nil {
+				if rs[i].ContractAddress == (common.Address{}) {
+					// Deriving the signer is expensive, only do if it's actually needed
+					from, _ := Sender(signer, txs[i])
+					rs[i].ContractAddress = crypto.CreateAddress(from, txs[i].Nonce())
+				}
 			} else {
 				rs[i].ContractAddress = common.Address{}
 			}
