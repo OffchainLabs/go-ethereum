getamis/arb-geth

*: fix misspelling and unnecessary conversion

core/types/transaction_marshalling.go
@@ -145,7 +145,7 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		enc.Value = (*hexutil.Big)(tx.Value)
 		enc.To = t.To()
 	case *ArbitrumUnsignedTx:
-		enc.From = (*common.Address)(&tx.From)
+		enc.From = &tx.From
 		enc.ChainID = (*hexutil.Big)(tx.ChainId)
 		enc.Nonce = (*hexutil.Uint64)(&tx.Nonce)
 		enc.Gas = (*hexutil.Uint64)(&tx.Gas)
@@ -155,7 +155,7 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		enc.To = t.To()
 	case *ArbitrumContractTx:
 		enc.RequestId = &tx.RequestId
-		enc.From = (*common.Address)(&tx.From)
+		enc.From = &tx.From
 		enc.ChainID = (*hexutil.Big)(tx.ChainId)
 		enc.Gas = (*hexutil.Uint64)(&tx.Gas)
 		enc.MaxFeePerGas = (*hexutil.Big)(tx.GasFeeCap)
@@ -163,7 +163,7 @@ func (t *Transaction) MarshalJSON() ([]byte, error) {
 		enc.Data = (*hexutil.Bytes)(&tx.Data)
 		enc.To = t.To()
 	case *ArbitrumRetryTx:
-		enc.From = (*common.Address)(&tx.From)
+		enc.From = &tx.From
 		enc.TicketId = &tx.TicketId
 		enc.RefundTo = &tx.RefundTo
 		enc.ChainID = (*hexutil.Big)(tx.ChainId)

OffchainLabs/go-ethereum

(cherry picked from commit e994cec799736a67fa95142993f0262f542d6298)
Reapplied with some conflict resolution around
transaction_marshalling.go

core/types/transaction_marshalling.go
@@ -145,7 +145,7 @@ func (tx *Transaction) MarshalJSON() ([]byte, error) {
 		enc.Value = (*hexutil.Big)(itx.Value)
 		enc.To = tx.To()
 	case *ArbitrumUnsignedTx:
-		enc.From = (*common.Address)(&itx.From)
+		enc.From = &itx.From
 		enc.ChainID = (*hexutil.Big)(itx.ChainId)
 		enc.Nonce = (*hexutil.Uint64)(&itx.Nonce)
 		enc.Gas = (*hexutil.Uint64)(&itx.Gas)
@@ -163,7 +163,7 @@ func (tx *Transaction) MarshalJSON() ([]byte, error) {
 		enc.Data = (*hexutil.Bytes)(&itx.Data)
 		enc.To = tx.To()
 	case *ArbitrumRetryTx:
-		enc.From = (*common.Address)(&itx.From)
+		enc.From = &itx.From
 		enc.TicketId = &itx.TicketId
 		enc.RefundTo = &itx.RefundTo
 		enc.ChainID = (*hexutil.Big)(itx.ChainId)