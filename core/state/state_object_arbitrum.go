9e23c3fce2d82ebc72d950ed4c3539224315f912
Raul Jordan <raul@prysmaticlabs.com>
2023-07-05 15:45:46 -0400

    rem print

--- core/state/state_object_arbitrum.go
+++ core/state/state_object_arbitrum.go
@@ -39,7 +39,6 @@ func (c CompiledWasms) Copy() CompiledWasms {
 
 // CompiledWasmCode returns the user wasm contract code associated with this object, if any.
 func (s *stateObject) CompiledWasmCode(db Database, version uint32) []byte {
-	fmt.Printf("Getting compiled code for %#x\n", s.address)
 	if wasm, ok := s.compiledWasmCode[version]; ok {
 		return wasm.code
 	}
