ce5229ef6315d40b4c01da9497cac75f42b64992
Charles Rodiger <61745898+charlie-paxos@users.noreply.github.com>
2024-07-17 16:59:32 -0400

    just don't export

--- crypto/secp256k1/panic_cb.go
+++ crypto/secp256k1/panic_cb.go
@@ -13,14 +13,10 @@ import "unsafe"
 // Callbacks for converting libsecp256k1 internal faults into
 // recoverable Go panics.
 
-//go:linkname secp256k1GoPanicIllegal secp256k1GoPanicIllegal
-//go:noinline
 func secp256k1GoPanicIllegal(msg *C.char, data unsafe.Pointer) {
 	panic("illegal argument: " + C.GoString(msg))
 }
 
-//go:linkname secp256k1GoPanicError secp256k1GoPanicError
-//go:noinline
 func secp256k1GoPanicError(msg *C.char, data unsafe.Pointer) {
 	panic("internal error: " + C.GoString(msg))
 }
