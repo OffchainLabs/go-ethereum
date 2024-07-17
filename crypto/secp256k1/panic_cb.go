d2df2aefc635405455ad4118eb4d17eb913a417c
Charles Rodiger <61745898+charlie-paxos@users.noreply.github.com>
2024-07-17 16:54:01 -0400

    remove noescape

--- crypto/secp256k1/panic_cb.go
+++ crypto/secp256k1/panic_cb.go
@@ -14,13 +14,11 @@ import "unsafe"
 // recoverable Go panics.
 
 //go:linkname secp256k1GoPanicIllegal secp256k1GoPanicIllegal
-//go:noescape
 func secp256k1GoPanicIllegal(msg *C.char, data unsafe.Pointer) {
 	panic("illegal argument: " + C.GoString(msg))
 }
 
 //go:linkname secp256k1GoPanicError secp256k1GoPanicError
-//go:noescape
 func secp256k1GoPanicError(msg *C.char, data unsafe.Pointer) {
 	panic("internal error: " + C.GoString(msg))
 }
