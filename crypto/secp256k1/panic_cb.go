e8d8fe005b3e79c6bbabd9eb1604c32d2f38cf3c
Charles Rodiger <61745898+charlie-paxos@users.noreply.github.com>
2024-07-17 16:57:25 -0400

    try without inlining

--- crypto/secp256k1/panic_cb.go
+++ crypto/secp256k1/panic_cb.go
@@ -14,11 +14,13 @@ import "unsafe"
 // recoverable Go panics.
 
 //go:linkname secp256k1GoPanicIllegal secp256k1GoPanicIllegal
+//go:noinline
 func secp256k1GoPanicIllegal(msg *C.char, data unsafe.Pointer) {
 	panic("illegal argument: " + C.GoString(msg))
 }
 
 //go:linkname secp256k1GoPanicError secp256k1GoPanicError
+//go:noinline
 func secp256k1GoPanicError(msg *C.char, data unsafe.Pointer) {
 	panic("internal error: " + C.GoString(msg))
 }
