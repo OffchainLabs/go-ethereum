0c3f6eba21cbe0196b298dfbd3fa7d51dffd627e
Maciej Kulawik <mkulawik@offchainlabs.com>
2024-09-26 16:50:58 +0200

    use MessageReplayMode in StateProcessor.Process

--- core/state_processor.go
+++ core/state_processor.go
@@ -80,7 +80,7 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	}
 	// Iterate over and process the individual transactions
 	for i, tx := range block.Transactions() {
-		msg, err := TransactionToMessage(tx, signer, header.BaseFee, MessageCommitMode)
+		msg, err := TransactionToMessage(tx, signer, header.BaseFee, MessageReplayMode)
 		if err != nil {
 			return nil, nil, 0, fmt.Errorf("could not apply tx %d [%v]: %w", i, tx.Hash().Hex(), err)
 		}
