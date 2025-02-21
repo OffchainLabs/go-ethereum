885daf475555e834a1c34442652e6668f33ac37f
Maciej Kulawik <mkulawik@offchainlabs.com>
2025-02-21 15:33:21 +0100

    remove no longer used exported functions

--- core/rawdb/schema.go
+++ core/rawdb/schema.go
@@ -167,17 +167,9 @@ func headerKey(number uint64, hash common.Hash) []byte {
 	return append(append(headerPrefix, encodeBlockNumber(number)...), hash.Bytes()...)
 }
 
-func HeaderKey(number uint64, hash common.Hash) []byte {
-	return headerKey(number, hash)
-}
-
 // headerTDKey = headerPrefix + num (uint64 big endian) + hash + headerTDSuffix
 func headerTDKey(number uint64, hash common.Hash) []byte {
 	return append(headerKey(number, hash), headerTDSuffix...)
-}
-
-func HeaderTDKey(number uint64, hash common.Hash) []byte {
-	return headerTDKey(number, hash)
 }
 
 // headerHashKey = headerPrefix + num (uint64 big endian) + headerHashSuffix
@@ -195,17 +187,9 @@ func blockBodyKey(number uint64, hash common.Hash) []byte {
 	return append(append(blockBodyPrefix, encodeBlockNumber(number)...), hash.Bytes()...)
 }
 
-func BlockBodyKey(number uint64, hash common.Hash) []byte {
-	return blockBodyKey(number, hash)
-}
-
 // blockReceiptsKey = blockReceiptsPrefix + num (uint64 big endian) + hash
 func blockReceiptsKey(number uint64, hash common.Hash) []byte {
 	return append(append(blockReceiptsPrefix, encodeBlockNumber(number)...), hash.Bytes()...)
-}
-
-func BlockReceiptsKey(number uint64, hash common.Hash) []byte {
-	return blockReceiptsKey(number, hash)
 }
 
 // txLookupKey = txLookupPrefix + hash
