59305f427f81212981200bd355858cd623572196
Joshua Colvin <jcolvin@offchainlabs.com>
2025-05-08 13:54:03 -0700

    Handle gas pool overflow gracefully
    eth_call sometimes overcompensates for L1 gas costs.  In that case, handle gracefully and log additional information

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -723,6 +723,9 @@ func applyMessage(ctx context.Context, b Backend, args TransactionArgs, state *s
 		}
 		if gp.Gas() < gomath.MaxUint64-postingGas {
 			gp.AddGas(postingGas)
+		} else {
+			log.Warn("gas pool overflow", "gpGas", gp.Gas(), "baseFee", header.BaseFee, "postingGas", postingGas, "msgDataLen", len(msg.Data))
+			gp.SetGas(gomath.MaxUint64)
 		}
 	}
 	if msg.GasLimit > gp.Gas() {
