df5fb1d9a8849e41c3edbbaada3142799453ab2f
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-07-02 11:33:47 -0300

    Improve error messages of unit tests

--- arbitrum/multigas/constraint_test.go
+++ arbitrum/multigas/constraint_test.go
@@ -5,6 +5,13 @@ import (
 	"time"
 )
 
+func assertEqual[T comparable](t *testing.T, description string, got T, want T) {
+	t.Helper()
+	if got != want {
+		t.Fatalf("unexpected %v: want %v, got %v", description, want, got)
+	}
+}
+
 func TestResourceConstraints(t *testing.T) {
 	rc := NewResourceConstraints()
 
@@ -19,54 +26,28 @@ func TestResourceConstraints(t *testing.T) {
 	rc.SetConstraint(ResourceKindComputation, minuteSecs, 5_000_000*minuteSecs)
 	rc.SetConstraint(ResourceKindComputation, weekSecs, 3_000_000*weekSecs)
 	rc.SetConstraint(ResourceKindHistoryGrowth, monthSecs, 1_000_000*monthSecs)
-	if len(rc[ResourceKindComputation]) != 2 {
-		t.Fatalf("expected 2 computation constraints")
-	}
-	if rc[ResourceKindComputation][minuteSecs].period != time.Duration(minuteSecs)*time.Second {
-		t.Errorf("wrong constraint period")
-	}
-	if rc[ResourceKindComputation][minuteSecs].target != 5_000_000 {
-		t.Errorf("wrong constraint target")
-	}
-	if rc[ResourceKindComputation][weekSecs].period != time.Duration(weekSecs)*time.Second {
-		t.Errorf("wrong constraint period")
-	}
-	if rc[ResourceKindComputation][weekSecs].target != 3_000_000 {
-		t.Errorf("wrong constraint target")
-	}
-	if len(rc[ResourceKindHistoryGrowth]) != 1 {
-		t.Fatalf("Expected 1 history growth constraint")
-	}
-	if rc[ResourceKindHistoryGrowth][monthSecs].period != time.Duration(monthSecs)*time.Second {
-		t.Errorf("wrong constraint period")
-	}
-	if rc[ResourceKindHistoryGrowth][monthSecs].target != 1_000_000 {
-		t.Errorf("wrong constraint target")
-	}
-	if len(rc[ResourceKindStorageAccess]) != 0 {
-		t.Fatalf("expected 0 storage access constraints")
-	}
-	if len(rc[ResourceKindStorageGrowth]) != 0 {
-		t.Fatalf("expected 0 storage growth constraints")
-	}
+	assertEqual(t, "number of computation constraints", len(rc[ResourceKindComputation]), 2)
+	assertEqual(t, "constraint period", rc[ResourceKindComputation][minuteSecs].period, time.Duration(minuteSecs)*time.Second)
+	assertEqual(t, "constraint target", rc[ResourceKindComputation][minuteSecs].target, 5_000_000)
+	assertEqual(t, "constraint period", rc[ResourceKindComputation][weekSecs].period, time.Duration(weekSecs)*time.Second)
+	assertEqual(t, "constraint target", rc[ResourceKindComputation][weekSecs].target, 3_000_000)
+	assertEqual(t, "number of history growth constraints", len(rc[ResourceKindHistoryGrowth]), 1)
+	assertEqual(t, "constraint period", rc[ResourceKindHistoryGrowth][monthSecs].period, time.Duration(monthSecs)*time.Second)
+	assertEqual(t, "constraint target", rc[ResourceKindHistoryGrowth][monthSecs].target, 1_000_000)
+	assertEqual(t, "number of storage access constraints", len(rc[ResourceKindStorageAccess]), 0)
+	assertEqual(t, "number of storage growth constraints", len(rc[ResourceKindStorageGrowth]), 0)
 
 	// Updates a constraint
 	rc.SetConstraint(ResourceKindHistoryGrowth, monthSecs, 500_000*monthSecs)
-	if len(rc[ResourceKindHistoryGrowth]) != 1 {
-		t.Fatalf("Expected 1 history growth constraints")
-	}
-	if rc[ResourceKindHistoryGrowth][monthSecs].target != 500_000 {
-		t.Errorf("wrong constraint target")
-	}
+	assertEqual(t, "number of history growth constraints", len(rc[ResourceKindHistoryGrowth]), 1)
+	assertEqual(t, "constraint target", rc[ResourceKindHistoryGrowth][monthSecs].target, 500_000)
 
 	// Clear constraints
 	rc.ClearConstraint(ResourceKindComputation, minuteSecs)
 	rc.ClearConstraint(ResourceKindComputation, weekSecs)
 	rc.ClearConstraint(ResourceKindHistoryGrowth, monthSecs)
-	if len(rc[ResourceKindComputation]) != 0 {
-		t.Fatalf("Expected 0 computation constraints")
-	}
-	if len(rc[ResourceKindHistoryGrowth]) != 0 {
-		t.Fatalf("Expected 0 history growth constraints")
-	}
+	assertEqual(t, "number of computation constraints", len(rc[ResourceKindComputation]), 0)
+	assertEqual(t, "number of history growth constraints", len(rc[ResourceKindHistoryGrowth]), 0)
+	assertEqual(t, "number of storage access constraints", len(rc[ResourceKindStorageAccess]), 0)
+	assertEqual(t, "number of storage growth constraints", len(rc[ResourceKindStorageGrowth]), 0)
 }
