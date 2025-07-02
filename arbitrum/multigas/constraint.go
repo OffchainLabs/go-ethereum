7380b4659722c303b6664fa85c9a2d9e1a72721e
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-07-02 11:23:47 -0300

    Replace uint32 with PeriodSecs type

--- arbitrum/multigas/constraint.go
+++ arbitrum/multigas/constraint.go
@@ -2,6 +2,9 @@ package multigas
 
 import "time"
 
+// The period duration for a resource constraint.
+type PeriodSecs uint32
+
 // resourceConstraint defines the max gas target per second for the given period for a single resource.
 type resourceConstraint struct {
 	period time.Duration
@@ -18,21 +21,21 @@ type resourceConstraint struct {
 // - X amount of computation over 12 seconds so nodes can keep up.
 // - Y amount of computation over 7 days so fresh nodes can catch up with the chain.
 // - Z amount of history growth over one month to avoid bloat.
-type ResourceConstraints map[ResourceKind]map[uint32]resourceConstraint
+type ResourceConstraints map[ResourceKind]map[PeriodSecs]resourceConstraint
 
 // NewResourceConstraints creates a new set of constraints.
 // This type can be used as a reference.
 func NewResourceConstraints() ResourceConstraints {
 	c := ResourceConstraints{}
 	for resource := ResourceKind(0); resource < NumResourceKind; resource++ {
-		c[resource] = map[uint32]resourceConstraint{}
+		c[resource] = map[PeriodSecs]resourceConstraint{}
 	}
 	return c
 }
 
 // SetConstraint adds or updates the given resource constraint.
 func (rc ResourceConstraints) SetConstraint(
-	resource ResourceKind, periodSecs uint32, targetPerPeriod uint64,
+	resource ResourceKind, periodSecs PeriodSecs, targetPerPeriod uint64,
 ) {
 	rc[resource][periodSecs] = resourceConstraint{
 		period: time.Duration(periodSecs) * time.Second,
@@ -41,6 +44,6 @@ func (rc ResourceConstraints) SetConstraint(
 }
 
 // ClearConstraint removes the given resource constraint.
-func (rc ResourceConstraints) ClearConstraint(resource ResourceKind, periodSecs uint32) {
+func (rc ResourceConstraints) ClearConstraint(resource ResourceKind, periodSecs PeriodSecs) {
 	delete(rc[resource], periodSecs)
 }
