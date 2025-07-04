e95f9dc372b8ac739db81c7f824dcd6a84d7e35c
Gabriel de Quadros Ligneul <gligneul@offchainlabs.com>
2025-07-04 09:23:09 -0300

    Add more methods to MultiGas struct

--- arbitrum/multigas/resources.go
+++ arbitrum/multigas/resources.go
@@ -13,4 +13,52 @@ const (
 )
 
 // MultiGas tracks gas for each resource separately.
-type MultiGas [NumResourceKind]uint64
+type MultiGas struct {
+	gas [NumResourceKind]uint64
+}
+
+func ZeroGas() *MultiGas {
+	return &MultiGas{}
+}
+
+func NewMultiGas(kind ResourceKind, amount uint64) *MultiGas {
+	mg := ZeroGas()
+	mg.gas[kind] = amount
+	return mg
+}
+
+func ComputationGas(amount uint64) *MultiGas {
+	return NewMultiGas(ResourceKindComputation, amount)
+}
+
+func HistoryGrowthGas(amount uint64) *MultiGas {
+	return NewMultiGas(ResourceKindHistoryGrowth, amount)
+}
+
+func StorageAccessGas(amount uint64) *MultiGas {
+	return NewMultiGas(ResourceKindStorageAccess, amount)
+}
+
+func StorageGrowthGas(amount uint64) *MultiGas {
+	return NewMultiGas(ResourceKindStorageGrowth, amount)
+}
+
+func (z *MultiGas) Get(kind ResourceKind) uint64 {
+	return z.gas[kind]
+}
+
+// Add sets z to the sum x+y and returns z.
+func (z *MultiGas) Add(x *MultiGas, y *MultiGas) *MultiGas {
+	for i := ResourceKindUnknown; i < NumResourceKind; i++ {
+		z.gas[i] = x.gas[i] + y.gas[i]
+	}
+	return z
+}
+
+// Sub sets z to the difference x-y and returns z.
+func (z *MultiGas) Sub(x *MultiGas, y *MultiGas) *MultiGas {
+	for i := ResourceKindUnknown; i < NumResourceKind; i++ {
+		z.gas[i] = x.gas[i] - y.gas[i]
+	}
+	return z
+}
