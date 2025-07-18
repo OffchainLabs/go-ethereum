83d8f13db509f3c2a5334daa09466c0df9574b49
Pepper Lebeck-Jobe <eljobe@gmail.com>
2025-07-18 13:21:26 +0200

    Revert "Merge pull request #482 from OffchainLabs/gligneul/gas-resources"
    This reverts commit 5f6f8694e4625b8d7d9b0b5dda44aeb30716a964, reversing
    changes made to 329d12fd75e42bacd3a0b15133143233b032fb07.

--- arbitrum/multigas/doc.go
+++
@@ -1,7 +0,0 @@
-// Package multigas defines multi-dimensional gas for the EVM.
-//
-// This package introduces mechanisms to track each resource used by the EVM separately. The
-// possible resources are computation, history growth, storage access, and storage growth. By
-// tracking each one individually and setting specific constraints, we can increase the overall gas
-// target for the chain.
-package multigas
