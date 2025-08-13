af8d94ad3a771df999d6633074fb360685ad43a4
Alexandros Filios <afilios@offchainlabs.com>
2025-08-13 16:53:06 +0200

    fixup! Enabled precompiled contracts of EIP-2537 in ArbOS50

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -104,23 +104,16 @@ var PrecompiledContractsBerlin = PrecompiledContracts{
 // PrecompiledContractsCancun contains the default set of pre-compiled Ethereum
 // contracts used in the Cancun release.
 var PrecompiledContractsCancun = PrecompiledContracts{
-	common.BytesToAddress([]byte{0x1}):  &ecrecover{},
-	common.BytesToAddress([]byte{0x2}):  &sha256hash{},
-	common.BytesToAddress([]byte{0x3}):  &ripemd160hash{},
-	common.BytesToAddress([]byte{0x4}):  &dataCopy{},
-	common.BytesToAddress([]byte{0x5}):  &bigModExp{eip2565: true},
-	common.BytesToAddress([]byte{0x6}):  &bn256AddIstanbul{},
-	common.BytesToAddress([]byte{0x7}):  &bn256ScalarMulIstanbul{},
-	common.BytesToAddress([]byte{0x8}):  &bn256PairingIstanbul{},
-	common.BytesToAddress([]byte{0x9}):  &blake2F{},
-	common.BytesToAddress([]byte{0xa}):  &kzgPointEvaluation{},
-	common.BytesToAddress([]byte{0x0b}): &bls12381G1Add{},
-	common.BytesToAddress([]byte{0x0c}): &bls12381G1MultiExp{},
-	common.BytesToAddress([]byte{0x0d}): &bls12381G2Add{},
-	common.BytesToAddress([]byte{0x0e}): &bls12381G2MultiExp{},
-	common.BytesToAddress([]byte{0x0f}): &bls12381Pairing{},
-	common.BytesToAddress([]byte{0x10}): &bls12381MapG1{},
-	common.BytesToAddress([]byte{0x11}): &bls12381MapG2{},
+	common.BytesToAddress([]byte{0x1}): &ecrecover{},
+	common.BytesToAddress([]byte{0x2}): &sha256hash{},
+	common.BytesToAddress([]byte{0x3}): &ripemd160hash{},
+	common.BytesToAddress([]byte{0x4}): &dataCopy{},
+	common.BytesToAddress([]byte{0x5}): &bigModExp{eip2565: true},
+	common.BytesToAddress([]byte{0x6}): &bn256AddIstanbul{},
+	common.BytesToAddress([]byte{0x7}): &bn256ScalarMulIstanbul{},
+	common.BytesToAddress([]byte{0x8}): &bn256PairingIstanbul{},
+	common.BytesToAddress([]byte{0x9}): &blake2F{},
+	common.BytesToAddress([]byte{0xa}): &kzgPointEvaluation{},
 }
 
 // PrecompiledContractsP256Verify contains the precompiled Ethereum
