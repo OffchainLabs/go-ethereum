a17b8eeedb9973202559c41f78f8d4e54158c2be
Aman Sanghi <asanghi@offchainlabs.com>
2025-08-28 12:32:59 +0530

    Skip block size cap for arbitrum chains

--- core/block_validator.go
+++ core/block_validator.go
@@ -50,7 +50,8 @@ func NewBlockValidator(config *params.ChainConfig, blockchain *BlockChain) *Bloc
 // validated at this point.
 func (v *BlockValidator) ValidateBody(block *types.Block) error {
 	// check EIP 7934 RLP-encoded block size cap
-	if v.config.IsOsaka(block.Number(), block.Time()) && block.Size() > params.MaxBlockSize {
+	// but skip for Arbitrum (since arbtrium does not need a block size cap)
+	if !v.config.IsArbitrum() && v.config.IsOsaka(block.Number(), block.Time()) && block.Size() > params.MaxBlockSize {
 		return ErrBlockOversized
 	}
 	// Check whether the block is already imported.
