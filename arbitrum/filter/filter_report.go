64d2cc0eb5f6400bbecdac4b981ee368db31a4a8
Mikhail Rogachev <mrogachev@offchainlabs.com>
2026-04-08 17:39:40 +0200

    Remove FilterSetID from FilteredAddressRecord and put back lint:require-exhaustive-initialization

--- arbitrum/filter/filter_report.go
+++ arbitrum/filter/filter_report.go
@@ -36,13 +36,14 @@ type EventRuleMatch struct {
 	RawLog            *RawLog `json:"rawLog,omitempty"`
 }
 
+// lint:require-exhaustive-initialization
 type FilterReason struct {
 	Reason FilterReasonType `json:"reason"`
 	*EventRuleMatch
 }
 
+// lint:require-exhaustive-initialization
 type FilteredAddressRecord struct {
-	Address     common.Address `json:"address"`
-	FilterSetID string         `json:"filterSetId"`
+	Address common.Address `json:"address"`
 	FilterReason
 }
